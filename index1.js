733356daa622b918963eef2672100f13629606e9
n21chis015 <118789954+n21chis015@users.noreply.github.com>
2022-11-24 14:10:54 +0700

    Update index1.js

--- index1.js
+++ index1.js
@@ -1,15 +1,6 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+var http = require('http');
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
-app.get("/", (req, res) => {
-  res.send(`Demo Hệ điều hành mạng: AWS VPS`);
-});
-
-app.get("/:name", (req, res) => {
-  res.send(`Học viên: ${req.params.name}`);
-});
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+}).listen(3000);
